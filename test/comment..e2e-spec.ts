eda84751b04400ff51c9c025e04591812b837914
Mohammad Foroutan <mmdfrn81@gmail.com>
2021-08-16 16:41:51 +0430

    init tests

--- test/comment..e2e-spec.ts
+++ test/comment..e2e-spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication, Logger, LoggerService } from '@nestjs/common';
+import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
 
@@ -17,13 +17,4 @@ afterAll((done) => {
   // Closing the connection allows Jest to exit successfully.
   app.close();
   done();
-});
-
-describe('AppController (e2e)', () => {
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
-  });
 });
