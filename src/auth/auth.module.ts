18baad2180691b32144206152caafc515a942957
Mohammad Foroutan <mmdfrn81@gmail.com>
2021-08-27 10:46:33 +0430

    change path

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -7,7 +7,7 @@ import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt.strategy';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TokenRepository } from 'src/token/token.repository';
+import { TokenRepository } from '../token/token.repository';
 
 @Module({
   imports: [
