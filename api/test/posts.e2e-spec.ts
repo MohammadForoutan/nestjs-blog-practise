65b0334ded6e6533792874e65b22235751cace2a
Mohammad Foroutan <mmdfrn81@gmail.com>
2021-09-05 17:51:33 +0430

    insert all files in api folder

---
+++ api/test/posts.e2e-spec.ts
@@ -0,0 +1,29 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from '../src/app.module';
+
+let app: INestApplication;
+beforeAll(async () => {
+  const moduleFixture: TestingModule = await Test.createTestingModule({
+    imports: [AppModule],
+  }).compile();
+
+  app = moduleFixture.createNestApplication();
+  await app.init();
+});
+
+afterAll((done) => {
+  // Closing the connection allows Jest to exit successfully.
+  app.close();
+  done();
+});
+
+describe('AppController (e2e)', () => {
+  it('/ (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/')
+      .expect(200)
+      .expect('Hello World!');
+  });
+});
